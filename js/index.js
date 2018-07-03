604144bbe21f82bfc01589b3e198b5ed2fa55619
clj4348 <1136284348@qq.com>
2018-07-03 22:16:44 +0800

    首页动画效果

--- js/index.js
+++ js/index.js
@@ -4,38 +4,99 @@
 * @Last Modified by:   Administrator
 * @Last Modified time: 2017-11-25 22:11:25
 */
-var menuButton = document.querySelector('.menu-button');
-var swiper = new Swiper('#slide-content', {
-  slidesPerView: 'auto',
-  initialSlide: 1,
-  resistanceRatio: 0,
-  slideToClickedSlide: true,
-  on: {
-    init: function () {
-      var slider = this;
-      menuButton.addEventListener('click', function () {
-        if (slider.activeIndex === 0) {
-          slider.slideNext();  
-        }else{
-          slider.slidePrev();
-        }
-      }, true);
-    },
-    slideChange: function () {
-      var slider = this;
-      if (slider.activeIndex === 0) {
-        menuButton.classList.add('cross');
-      } else {
-        menuButton.classList.remove('cross');
-      }
-    },
+
+// var swiper = new Swiper('#slide-content', {
+//   slidesPerView: 'auto',
+//   initialSlide: 1,
+//   resistanceRatio: 0,
+//   slideToClickedSlide: true,
+//   on: {
+//     init: function () {
+//       var slider = this;
+//       menuButton.addEventListener('click', function () {
+//         if (slider.activeIndex === 0) {
+//           slider.slideNext();  
+//         }else{
+//           slider.slidePrev();
+//         }
+//       }, true);
+//     },
+//     slideChange: function () {
+//       var slider = this;
+//       if (slider.activeIndex === 0) {
+//         menuButton.classList.add('cross');
+//       } else {
+//         menuButton.classList.remove('cross');
+//       }
+//     },
+//   }
+// });
+// var swiperPage = new Swiper('#pageContainer', {
+//   direction: 'vertical',
+//   mousewheel:true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
+
+class index {
+  init(){
+    this.bindEvent();
   }
-});
-var swiperPage = new Swiper('#pageContainer', {
-  direction: 'vertical',
-  mousewheel:true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-});
+  bindEvent(){
+    let swiperPage = new Swiper('#pageContainer', {
+      slidesPerView: 'auto',
+      initialSlide: 0,
+      resistanceRatio: 0,
+      slideToClickedSlide: true,
+      direction: 'vertical',
+      mousewheel:true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      on:{
+            init: function(){
+              var slider = this;
+              swiperAnimateCache(this); //隐藏动画元素 
+              swiperAnimate(this); //初始化完成开始动画
+            },
+            slideChangeTransitionEnd: function(swiperPage){ 
+              swiperAnimate(this);
+          } 
+        }
+    });
+    let menuButton = document.querySelector('.menu-button');
+    let swiperSlidrBar = new Swiper('#slide-content', {
+    slidesPerView: 'auto',
+    initialSlide: 1,
+    resistanceRatio: 0,
+    slideToClickedSlide: true,
+    on: {
+      init: function () {
+        var slider = this;
+        menuButton.addEventListener('click', function () {
+          if (slider.activeIndex === 0) {
+            slider.slideNext();  
+          }else{
+            slider.slidePrev();
+          }
+        }, true);
+      },
+      slideChange: function () {
+        var slider = this;
+        if (slider.activeIndex === 0) {
+          menuButton.classList.add('cross');
+        } else {
+          menuButton.classList.remove('cross');
+        }
+      },
+    }
+  });
+  }
+}
+$(function(){
+  let home = new index()
+  home.init();
+})
