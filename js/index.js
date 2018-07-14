f4c196bcae98bfbcbb1770aba5af7adb7623d509
clj4348 <1136284348@qq.com>
2018-07-14 18:17:06 +0800

    联系我的页面

--- js/index.js
+++ js/index.js
@@ -5,40 +5,6 @@
 * @Last Modified time: 2017-11-25 22:11:25
 */
 
-// var swiper = new Swiper('#slide-content', {
-//   slidesPerView: 'auto',
-//   initialSlide: 1,
-//   resistanceRatio: 0,
-//   slideToClickedSlide: true,
-//   on: {
-//     init: function () {
-//       var slider = this;
-//       menuButton.addEventListener('click', function () {
-//         if (slider.activeIndex === 0) {
-//           slider.slideNext();  
-//         }else{
-//           slider.slidePrev();
-//         }
-//       }, true);
-//     },
-//     slideChange: function () {
-//       var slider = this;
-//       if (slider.activeIndex === 0) {
-//         menuButton.classList.add('cross');
-//       } else {
-//         menuButton.classList.remove('cross');
-//       }
-//     },
-//   }
-// });
-// var swiperPage = new Swiper('#pageContainer', {
-//   direction: 'vertical',
-//   mousewheel:true,
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-// });
 
 class index {
   init(){
