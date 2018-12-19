f2f5cb09badfc6a01d1d40a3880b9ef36051f92e
clj4348 <1136284348@qq.com>
2018-12-20 00:32:31 +0800

    文章

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ class index {
   bindEvent(){
     let swiperPage = new Swiper('#pageContainer', {
       slidesPerView: 'auto',
-      initialSlide: 0,
+      initialSlide: 4,
       resistanceRatio: 0,
       slideToClickedSlide: true,
       direction: 'vertical',
@@ -23,42 +23,42 @@ class index {
         clickable: true,
       },
       on:{
-            init: function(){
-              var slider = this;
-              swiperAnimateCache(this); //隐藏动画元素 
-              swiperAnimate(this); //初始化完成开始动画
-            },
-            slideChangeTransitionEnd: function(swiperPage){ 
-              swiperAnimate(this);
-          } 
-        }
+        init: function(){
+          var slider = this;
+          swiperAnimateCache(this); //隐藏动画元素 
+          swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(swiperPage){ 
+          swiperAnimate(this);
+        } 
+      }
     });
     let menuButton = document.querySelector('.menu-button');
     let swiperSlidrBar = new Swiper('#slide-content', {
-    slidesPerView: 'auto',
-    initialSlide: 1,
-    resistanceRatio: 0,
-    slideToClickedSlide: true,
-    on: {
-      init: function () {
-        var slider = this;
-        menuButton.addEventListener('click', function () {
+      slidesPerView: 'auto',
+      initialSlide: 1,
+      resistanceRatio: 0,
+      slideToClickedSlide: true,
+      on: {
+        init: function () {
+          var slider = this;
+          menuButton.addEventListener('click', function () {
+            if (slider.activeIndex === 0) {
+              slider.slideNext();  
+            }else{
+              slider.slidePrev();
+            }
+          }, true);
+        },
+        slideChange: function () {
+          var slider = this;
           if (slider.activeIndex === 0) {
-            slider.slideNext();  
-          }else{
-            slider.slidePrev();
+            menuButton.classList.add('cross');
+          } else {
+            menuButton.classList.remove('cross');
           }
-        }, true);
-      },
-      slideChange: function () {
-        var slider = this;
-        if (slider.activeIndex === 0) {
-          menuButton.classList.add('cross');
-        } else {
-          menuButton.classList.remove('cross');
-        }
-      },
-    }
+        },
+      }
   });
   }
 }
